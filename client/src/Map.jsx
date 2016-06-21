d6df3e55c0416f4fde1cb86e5ff010559e3be99e
Connie Cheung <conniedcheung@gmail.com>
2016-06-21 08:55:30 -0700

    Display Mapbox map on page via script in html, hit SODA API and can console.log results

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
-class Map extends React.Component {
+class MyMap extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div id='map'>
-        <h1>Map</h1>
+      <div id="mapid">
       </div>
     );
   }
 };
 
-export default Map;
+export default MyMap;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import SearchBar from './SearchBar.jsx';
 import Map from './Map.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    let filteredResults = {};
+    const endpoint = 'https://data.sfgov.org/resource/wwmu-gmzc.json';
+    $.get(endpoint, (results, error) => {
+      console.log('results: ', results);
+      results.forEach(result => {
+      });
+    });
   }
 
   render() {
