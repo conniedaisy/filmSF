35b53b232695abbb8b9d762dd478b8133aa0738a
Connie Cheung <conniedcheung@gmail.com>
2016-06-21 21:53:48 -0700

    Add webpack-dev-config.js and install webpack middlewares

---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/src/App.jsx'
+  ],
+
+  output: {
+    path: './client/dist',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [ "react-hot", "babel-loader" ]
+      },
+      {
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      }
+    ]
+  }
+};
