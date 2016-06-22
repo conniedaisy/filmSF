5f1582410385ce3053922d2cd2ed49cac06e0c8d
Connie Cheung <conniedcheung@gmail.com>
2016-06-22 07:54:56 -0700

    Add tokens for deployment

---
+++ server/accessTokens.js
@@ -0,0 +1,6 @@
+const accessTokens = {
+  SODA_api: 'pk.eyJ1IjoiY29ubmllZGFpc3kiLCJhIjoiY2lwb21ibHJvMDIwZGZ4bm82cXJyaHVhdyJ9.daSakXj3HSGIstl26_x5DQ',
+  GooglePlaces_api: 'AIzaSyABy0XtKmM7ad95t8p5osGHgVv0gUyREOE',
+};
+
+module.exports = accessTokens;
